b6fbe9cd0f2bb7ac736461faa15a5d737ebb949a
Nourhan Sowar <48545560+NourhanSowar@users.noreply.github.com>
2020-02-24 14:04:06 +0200

    Add files via upload

---
+++ UDP _STREAM_SERVER.py
@@ -0,0 +1,50 @@
+import socket
+import numpy as np
+import cv2 as cv
+
+
+### Constants
+PEER_ID = 1
+UDP_SERVER_PORT = 9000
+TCP_SERVER_PORT = 0     #fixed for all peers
+UDP_BroadCast_PORT = 2000 #fixed for all peers
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+
+# Set a timeout so the socket does not block
+# indefinitely when trying to receive data.
+server.settimeout(0.2)
+server.bind(("", UDP_SERVER_PORT))
+message = "done".encode('utf-8')
+
+#addr = ("127.0.0.1", 65534)
+Buffer_size = 512
+width = 640
+height = 480
+cap = cv.VideoCapture(0)
+cap.set(3, width)
+cap.set(4, height)
+code = 'start'
+code = ('start' + (Buffer_size - len(code)) * 'a').encode('utf-8')
+
+
+if __name__ == '__main__':
+    #s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret:
+            server.sendto(code, ('<broadcast>', UDP_BroadCast_PORT))
+            data = frame.tostring()
+            for i in range(0, len(data), Buffer_size):
+                server.sendto(data[i:i+Buffer_size],  ('<broadcast>', UDP_BroadCast_PORT))
+            # cv.imshow('send', frame)
+            # if cv.waitKey(1) & 0xFF == ord('q'):
+                # break
+        else:
+            break
+    # s.close()
+    # cap.release()
+    # cv.destroyAllWindows()
---
+++ UDP_STREAM_CLIENT..py
@@ -0,0 +1,50 @@
+import socket
+import numpy as np
+import cv2 as cv
+
+
+
+
+### Constants
+PEER_ID = 1
+UDP_SERVER_PORT = 9000
+TCP_SERVER_PORT = 0     #fixed for all peers
+UDP_BroadCast_PORT = 2000 #fixed for all peers
+
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+client.bind(("", UDP_BroadCast_PORT ))
+
+#addr = ("127.0.0.1", 65534)
+Buffer_size = 512
+width = 640
+height = 480
+code = b'start'
+num_of_chunks = width * height * 3 / Buffer_size
+
+if __name__ == '__main__':
+    #s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #s.bind(addr)
+    while True:
+        chunks = []
+        start = False
+        while len(chunks) < num_of_chunks:
+            chunk, _ = client.recvfrom(Buffer_size)
+            if start:
+                chunks.append(chunk)
+            elif chunk.startswith(code):
+                start = True
+
+        byte_frame = b''.join(chunks)
+
+        frame = np.frombuffer(
+            byte_frame, dtype=np.uint8).reshape(height, width, 3)
+
+        cv.imshow('recv', frame)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    client.close()
+    cv.destroyAllWindows()
